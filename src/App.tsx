decbeb92046adc3f383ebb6d7dbab3cfdd030bfa
Fadly Tanjung <tanjungfadly96@gmail.com>
2025-06-27 21:57:21 +0800

    feat: integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,43 @@
+import useSWR from 'swr'
 import './App.css'
 
+const fetcher = (args: any) => fetch(args).then(res => res.json())
+
+function PokemonCard({ url, name }: { name: string; url: string; }) {
+  const { data: detail, error: detailErr } = useSWR(url, fetcher)
+
+  if (detailErr) return <div>Error loading {name}</div>
+  if (!detail) return <div>Loading {name}…</div>
+
+  const imgUrl = detail.sprites.other.dream_world.front_default
+
+  return (
+    <div className="card">
+      <img alt={name} src={imgUrl} />
+      <h4>{name}</h4>
+      <p>Weight: {detail.weight}</p>
+    </div>
+  )
+}
+
 function App() {
+  const { data, error } = useSWR(
+    'https://pokeapi.co/api/v2/pokemon?offset=0&limit=10',
+    fetcher
+  )
+
+  if (error) return <div>Failed to load Pokémon list.</div>
+  if (!data) return <div>Loading list…</div>
+
   return (
     <>
       <header>
-        Pokemon App
+        Pokémon App
       </header>
       <main>
-        <div className="card">
-          <img alt="sample 1" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" />
-          <h4>Pokemon 1</h4>
-          <p>Deskripsi Pokemon 1</p>
-        </div>
-        <div className="card">
-          <img alt="sample 1" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" />
-          <h4>Pokemon 1</h4>
-          <p>Deskripsi Pokemon 1</p>
-        </div>
-        <div className="card">
-          <img alt="sample 1" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" />
-          <h4>Pokemon 1</h4>
-          <p>Deskripsi Pokemon 1</p>
-        </div>
-        <div className="card">
-          <img alt="sample 1" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" />
-          <h4>Pokemon 1</h4>
-          <p>Deskripsi Pokemon 1</p>
-        </div>
+        {data.results.map((p: any) => (
+          <PokemonCard key={p.name} url={p.url} name={p.name} />
+        ))}
       </main>
     </>
   )
